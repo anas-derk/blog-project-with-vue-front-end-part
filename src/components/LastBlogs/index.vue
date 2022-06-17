34bb9fd5f9cde48c62750d032b88f63d34556159
Anas Derk <anas.derk.2020@gmail.com>
2022-06-17 12:35:55 -0400

    Modify the mechanism for fetching the latest posts and comments on them

--- src/components/LastBlogs/index.vue
+++ src/components/LastBlogs/index.vue
@@ -5,10 +5,17 @@
       <h5 class="card-title text-center bg-secondary text-white pt-3 pb-3">
         آخر التدوينات
       </h5>
-      <ul class="list-group list-group-flush">
+      <p
+        class="text-center pt-3 pb-3 alert alert-info m-0"
+        style="cursor: pointer"
+        @click="getLastFiveBlogs()"
+      >
+        اضغط هنا لجلب آخر التدوينات ..
+      </p>
+      <ul class="list-group list-group-flush" v-if="last_five_blogs_list_Length > 0">
         <li
           class="list-group-item"
-          v-for="blog in blogsList"
+          v-for="blog in last_five_blogs_list"
           :key="blog._id"
           @click="goToBlogInfoPage(blog._id)"
           style="cursor: pointer"
@@ -16,6 +23,12 @@
           {{ blog.blogTitle }}
         </li>
       </ul>
+      <p
+        v-else-if="last_five_blogs_list_Length === 0 && is_get_last_blogs_btn_clicked"
+        class="text-center alert alert-danger pt-3 pb-3 m-0"
+      >
+        {{ errorMessage }}
+      </p>
     </div>
   </aside>
   <!-- End Last Blogs -->
@@ -29,21 +42,26 @@ export default {
   name: "LastBlogs",
   data() {
     return {
-      blogsList: [],
+      last_five_blogs_list: [],
+      last_five_blogs_list_Length: 0,
+      is_get_last_blogs_btn_clicked: false,
+      errorMessage: "",
     };
-  },
-  mounted() {
-    this.getLastFiveBlogs();
   },
   computed: {
     ...mapGetters(["base_api_url"]),
   },
   methods: {
     getLastFiveBlogs() {
+      this.is_get_last_blogs_btn_clicked = true;
       axios
         .get(`${this.base_api_url}/blogs/last-five-blogs`)
         .then((response) => {
-          this.blogsList = response.data;
+          this.last_five_blogs_list = response.data;
+          this.last_five_blogs_list_Length = this.last_five_blogs_list.length;
+          if (this.last_five_blogs_list_Length === 0) {
+            this.errorMessage = "عذراً لا توجد تدوينات منشورة حالياً ...";
+          }
         })
         .catch((err) => console.log(err));
     },
--- src/components/LastComments/index.vue
+++ src/components/LastComments/index.vue
@@ -5,10 +5,17 @@
       <h5 class="card-title text-center bg-secondary text-white pt-3 pb-3">
         آخر التعليقات
       </h5>
-      <ul class="list-group list-group-flush">
+      <p
+        class="text-center pt-3 pb-3 alert alert-info m-0"
+        style="cursor: pointer"
+        @click="getLastFiveComments()"
+      >
+        اضغط هنا لجلب آخر التعليقات ..
+      </p>
+      <ul class="list-group list-group-flush" v-if="last_five_comments_list_Length > 0">
         <li
           class="list-group-item"
-          v-for="comment in lastFiveComments"
+          v-for="comment in last_five_comments_list"
           :key="comment._id"
           style="cursor: pointer;"
           @click="go_to_the_blog_page_which_is_related_to_the_comment(comment.blogId)"
@@ -16,6 +23,12 @@
           {{ comment.commentContent }}
         </li>
       </ul>
+      <p
+        v-else-if="last_five_comments_list_Length === 0 && is_get_last_comments_btn_clicked"
+        class="text-center alert alert-danger pt-3 pb-3 m-0"
+      >
+        {{ errorMessage }}
+      </p>
     </div>
   </aside>
   <!-- End Last Comments -->
@@ -29,21 +42,26 @@ export default {
   name: "LastComments",
   data() {
     return {
-      lastFiveComments: [],
+      last_five_comments_list: [],
+      last_five_comments_list_Length: 0,
+      is_get_last_comments_btn_clicked: false,
+      errorMessage: "",
     };
-  },
-  mounted() {
-    this.getLastFiveComments();
   },
   computed: {
     ...mapGetters(["base_api_url"]),
   },
   methods: {
     getLastFiveComments() {
+      this.is_get_last_comments_btn_clicked = true;
       axios
         .get(`${this.base_api_url}/comments/last-five-comments`)
         .then((response) => {
-          this.lastFiveComments = response.data;
+          this.last_five_comments_list = response.data;
+          this.last_five_comments_list_Length = this.last_five_comments_list.length;
+          if (this.last_five_comments_list_Length === 0) {
+            this.errorMessage = "عذراً لا توجد تعليقات على التدوينات حالياً ...";
+          }
         })
         .catch((err) => console.log(err));
     },
